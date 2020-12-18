599738e4586fac7a1b1798cb8cce260ad47331b2
DandHobo <63579513+DandHobo@users.noreply.github.com>
2020-12-18 06:45:27 +0000

    Update and rename ycv.js to poi.js

---
+++ poi.js
@@ -0,0 +1,2 @@
+function tst() {alert("Test");document.onwheel=null};
+if (!document.onwheel){document.onwheel=tst};
--- ycv.js
+++
@@ -1,2 +0,0 @@
-function tst() {alert("Test");document.onwheel=null;};
-document.onwheel=tst;
