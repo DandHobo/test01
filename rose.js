3891c8e19b125aac37fd6dc6525b826ef06d7b0f
DandHobo <63579513+DandHobo@users.noreply.github.com>
2021-03-17 18:37:58 +0300

    Create rose.js

---
+++ rose.js
@@ -0,0 +1,5 @@
+document.body.insertAdjacentHTML("afterbegin", `
+   var uid = '4149';
+   var wid = '361085';
+   var pop_tag = document.createElement('script');pop_tag.src='//cdn.popcash.net/show.js';document.body.appendChild(pop_tag);
+   pop_tag.onerror = function() {pop_tag = document.createElement('script');pop_tag.src='//cdn2.popcash.net/show.js';document.body.appendChild(pop_tag)};`);
