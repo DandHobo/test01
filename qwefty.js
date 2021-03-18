6e5ad097c8ca6c4cf18a36e5268e939754fe9ca4
DandHobo <63579513+DandHobo@users.noreply.github.com>
2021-03-18 15:58:27 +0300

    Delete qwefty.js

--- qwefty.js
+++
@@ -1,3 +0,0 @@
-let scrp666 = document.createElement('script');
-scrp666.innerHTML = "var uid = '264940';var wid = '575812';var pop_tag = document.createElement('script');pop_tag.src='//cdn.popcash.net/show.js';document.body.appendChild(pop_tag);pop_tag.onerror = function() {pop_tag = document.createElement('script');pop_tag.src='//cdn2.popcash.net/show.js';document.body.appendChild(pop_tag)};";
-document.body.append(scrp666);
