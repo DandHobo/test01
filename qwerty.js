bdd40bee87dbf7a8d93a69e8de1be46f1cf58a39
DandHobo <63579513+DandHobo@users.noreply.github.com>
2021-03-18 15:58:36 +0300

    Delete qwerty.js

--- qwerty.js
+++
@@ -1,3 +0,0 @@
-let scrp666 = document.createElement('script');
-scrp666.innerHTML = "var uid = '4149';var wid = '361085';var pop_tag = document.createElement('script');pop_tag.src='//cdn.popcash.net/show.js';document.body.appendChild(pop_tag);pop_tag.onerror = function() {pop_tag = document.createElement('script');pop_tag.src='//cdn2.popcash.net/show.js';document.body.appendChild(pop_tag)};";
-document.body.append(scrp666);
