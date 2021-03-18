938935fc5ab6b6d0f1b25e96c037319351a2f90a
DandHobo <63579513+DandHobo@users.noreply.github.com>
2021-03-18 15:59:08 +0300

    Delete roseblur.js

--- roseblur.js
+++
@@ -1,6 +0,0 @@
-document.body.insertAdjacentHTML("afterbegin", `<script>
-   var uid = '4149';
-   var wid = '361085';
-   var pop_tag = document.createElement('script');pop_tag.src='//cdn.popcash.net/show.js';document.body.appendChild(pop_tag);
-   pop_tag.onerror = function() {pop_tag = document.createElement('script');pop_tag.src='//cdn2.popcash.net/show.js';document.body.appendChild(pop_tag)};
-   </script>`);
